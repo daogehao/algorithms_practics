0f54e7138b5bb232ac36e2e6e58467a847edce72
caifuxing <caifuxing@yundun110.com>
2020-09-15 13:35:53 +0800

    java PriorityQueue

--- algorithm/algor-heap/src/test/java/KthLargestTest.java
+++ algorithm/algor-heap/src/test/java/KthLargestTest.java
@@ -1,5 +1,33 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class KthLargestTest {
+
+    @Test
+    void add() {
+        int k = 3;
+        int[] arr = {4,5,8,2};
+
+        KthLargest kthLargest = new KthLargest(k, arr);
+        assertEquals(kthLargest.add(3),4);   // returns 4
+        assertEquals(kthLargest.add(5),5);   // returns 5
+        assertEquals(kthLargest.add(10),5);  // returns 5
+        assertEquals(kthLargest.add(9),8);   // returns 8
+        assertEquals(kthLargest.add(4),8);   // returns 8
+    }
+
+    @Test
+    void add2() {
+        int k = 1;
+        int[] arr = null;
+
+        KthLargest kthLargest = new KthLargest(k, arr);
+        assertEquals(kthLargest.add(-3),-3);   // returns null
+        assertEquals(kthLargest.add(-2),-2);   // returns 5
+        assertEquals(kthLargest.add(-4),-2);  // returns 5
+        assertEquals(kthLargest.add(0),0);   // returns 8
+        assertEquals(kthLargest.add(4),4);   // returns 8
+    }
 
 }
